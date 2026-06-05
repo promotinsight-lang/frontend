d8ecef3489b428057e382fb9e702cb3e3c4000e8
Jaman <witchbella17@gmail.com>
2026-06-05 18:41:14 +0600

    update

--- frontend/src/pages/BlogDetails.jsx
+++ frontend/src/pages/BlogDetails.jsx
@@ -45,7 +45,7 @@ export default function BlogDetails() {
             <div className="space-y-3">
               <div className="h-4 bg-gray-200 rounded w-full"></div>
               <div className="h-4 bg-gray-200 rounded w-5/6"></div>
-              <div className="h-4 bg-gray-200 rounded w-4/6"></div>
+              <div className="h-4 bg-gray-200 rounded w-4/6"></div> 
             </div>
           </div>
         ) : !blog ? (
--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -78,4 +78,4 @@ export default function Blogs() {
       </main>
     </div>
   );
-}
+} 
